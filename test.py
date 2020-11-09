fe1f80b0e6f1e01cc25956e7f00dc8a010df088a
Rachelle <rachellecha@users.noreply.github.com>
2020-11-08 22:07:39 -0500

    trying xgboost

---
+++ test.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import pandas as pd
+import numpy as np
+
+hello = tf.constant('Hello, TensorFlow!')
+hello.numpy()
